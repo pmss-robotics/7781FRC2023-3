7d8ab467fc84fd92dab5a4136e688eff539037a7
Amir <iknubs@gmail.com>
2023-02-21 21:25:20 -0800

    Moving drive ports to constants file, basic way to set motor to break mode added in comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@ public final class Constants {
     //Ports
     public static final int xboxControllerPort = 0;
 
+    public static final int frontLeftMotorPort = 2;
+    public static final int frontRightMotorPort = 3;
+    public static final int rearLeftMotorPort = 1;
+    public static final int rearRightMotorPort = 4;
+
 
     //Robot configs
     public static final double driveSensitivity = 0.3;
