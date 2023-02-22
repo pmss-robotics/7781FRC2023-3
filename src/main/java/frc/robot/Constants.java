3cfd832cb25f85b44d39dfad1230e4190ff0e2b4
Amir <iknubs@gmail.com>
2023-02-21 21:07:51 -0800

    Adding magic numbers to constants file, adding documention to some functions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
 public final class Constants {
-    
+    //Ports
+    public static final int xboxControllerPort = 0;
+
+
+    //Robot configs
+    public static final double driveSensitivity = 0.3;
 }
