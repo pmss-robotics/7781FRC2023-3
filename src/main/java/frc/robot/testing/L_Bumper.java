8a42ecacec63d0156853b85d0e964bc54f82a8e9
MysteriousGrimReaper <pmssroboticsclub@gmail.com>
2023-02-28 18:15:09 -0800

    ??? idk

--- src/main/java/frc/robot/testing/L_Bumper.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.testing;
-
-//Emergancy Breaks 
-public class L_Bumper {
-    
-}if(!_gamepad.getLeftBumper()){driveTrain.drivePeriodic(backforth,leftright);}else{
-// left bumper button to brake
-driveTrain.driveBrake();}
--- src/main/java/frc/robot/testing/R_Joystick.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.testing;
-// Arm lift 
-
-public class R_Joystick {
-
-}lift=Deadband(lift);
-// sensitivity * lift
-lift=Constants.armLiftSensitivity*lift;
-double open = Deadband(gRightT - 0.5) * sensitivity;
